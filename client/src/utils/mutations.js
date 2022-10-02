6ff59f1a8ee794ba2b1a9856ce7a047d5fcc62f2
monineathsar <monineathsar@gmail.com>
2022-10-01 19:30:16 -0700

    Added graphQL from client side

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,60 @@
+import { gql } from '@apollo/client';
+
+export const CREATE_USER = gql`
+    mutation createUser($username: String!, $email: String, $password: String!) {
+        createUser(username: $username, email: $email, password: $password) {
+            token
+            user {
+                _id
+                username
+                email
+            }
+        }
+    }
+`;
+
+export const LOGIN_USER = gql`
+    mutation login($email: String!, $password: String!) {
+        loginUser(email: $email, password: $password) {
+            token
+            user {
+                _id
+                username
+            }
+        }
+    }
+`;
+
+export const SAVE_BOOK = gql`
+    mutation saveBook($book: SaveBookInput!) {
+        saveBook(book: $book) {
+            username
+            email
+            saveBook {
+                authors
+                description
+                bookId
+                image
+                link
+                title
+            }
+        }        
+    }
+`;
+
+export const DELETE_BOOK = gql`
+    mutation deleteBook($bookId: ID!) {
+        deleteBook(bookId: $bookId) {
+            username
+            email
+            saveBook {
+                authors
+                description
+                bookId
+                image
+                link
+                title
+            }
+        }        
+    }
+`;
